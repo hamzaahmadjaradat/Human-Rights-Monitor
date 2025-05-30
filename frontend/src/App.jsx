c05aab4dfa5ad581a08e9e5ef96351d810f61b16
hamzajaradat <hamzajaradat87@gmail.com>
2025-05-30 20:01:18 +0300

    initial commit : build structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Human Rights Case Management</h1>
+      <p>This dashboard will let you manage, view, and track human rights cases.</p>
+    </div>
+  );
+}
+
+export default App;
