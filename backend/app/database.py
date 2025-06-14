4b9c115b21b833109037a4af9c45fff43d0bf07d
Taqi Adnan Najjar <164814180+TaqiAdnan@users.noreply.github.com>
2025-06-14 16:31:23 +0300

    DB

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,9 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["human_rights_monitor"]
-cases_collection = db["cases"]
-case_status_history_collection = db["case_status_history"]
-individuals_collection = db["individuals"]
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["human_rights_mis"]
